5c8102a7655769045c0601cc45b05e37192dd782
Jackbennett <jbennett@birkdalehigh.co.uk>
2016-07-11 12:50:57 +0100

    pulled in more students work

--- public/sketch-6.js
+++ public/sketch-6.js
@@ -1,22 +1,39 @@
 //title: Sketch 6: Rainbow edited by Student
 console.log('sketch-6')
-var barWidth = 20;
+
+var barWidth = 0.5;
+var barlength = 0.5;
 var earlybar = 0;
+var col = 10
 
 function setup() {
-  createCanvas(720, 400);
-  colorMode(HSB, height, 255, 255);  
+  createCanvas(972, 540);
+  colorMode(HSB, 255, 255, 255);  
   noStroke();
-  background(100, 400, 52);
+  background(0, 0, 0);
 }
 
 function draw() {
-  var whichBar = mouseX / barWidth;     //works out which bar you`re at
-  if (whichBar !== earlybar) {           //if the bar is not the last pressed then run the below loop
+  if (keyIsDown(UP_ARROW)) {
+    fill(255, 100, 0);             //to fill the circle to a a colour based on pos in the Y-axis  
+    ellipse(barX, mouseY, 20, 20);    //barx is on x axis, 0 is bottom on y axis, barwidth is the width of rectangle, height as in y axis max
+  }
+  if (col > 255)
+    col = col - 255
+    var whichBar = mouseX / barWidth;     //works out which bar you`re at
+  if (true) {           //if the bar is not the last pressed then run the below loop
+    col = col + 3
     console.log(height)
     var barX = whichBar * barWidth;     //
-    fill(mouseY, 255, 255);             //to fill the bar to a a colour based on pos in the Y-axis  
-    ellipse(barX, mouseY, barWidth, 20);    //barx is on x axis, 0 is bottom on y axis, barwidth is the width of rectangle, height as in y axis max
-    earlybar = whichBar;                //
+    fill(col, 255, 255);             //to fill the bar to a a colour based on pos in the Y-axis  
+    ellipse(barX, mouseY, 20, 20);    //barx is on x axis, 0 is bottom on y axis, barwidth is the width of rectangle, height as in y axis max
+    earlybar = whichBar
   }
+}
+function mouseReleased() {
+  noLoop();
+}
+
+function mousePressed()  {
+  loop();
 }
