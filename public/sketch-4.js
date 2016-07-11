e0e6ef923750d5c30071b2a9d07967ef9d05102c
Jackbennett <jbennett@birkdalehigh.co.uk>
2016-07-11 12:27:34 +0100

    Custom timer colors

--- public/sketch-4.js
+++ public/sketch-4.js
@@ -2,21 +2,36 @@
 console.log('sketch-4')
 
 var canvas
-var countdown = 10000 // milliseconds to count down.
+var countdown = 150000 // milliseconds to count down.
+var starttime = 0
 // To test, this starts from 70 seconds so whe get to handle the minutes transition.
 
 function setup() {
   canvas = createCanvas(400,400)
-  colorMode(HSB, 100)
-  textSize(96)
-  textAlign(CENTER, CENTER)
+  colorMode(HSB, 95)
 }
 
 function draw() {
-  var time = x = Math.round((countdown - millis())/1000)  
-  background(time, 100, 100)
-  fill(0, 0, 0)
-  strokeWeight(10)
-  stroke(0,0,100)
-  text(time, width / 2 , height / 2)
+  if (true) {
+    background(32, 100, 100)
+    var time = x = Math.round((countdown + starttime - millis())/1000)
+    if (time < 11) {
+      background(time, 100, 100)
+      }
+    fill(0, 0, 77)
+    }
+    if (time > -1) {
+      colorMode(HSB);
+      fill(0, 0, 0);
+      textSize(30);
+      text(time, width / 2 , height / 2)
+    }
+  }
+function mousePressed() {
+  if (millis() !== 0)  {
+    time = time + countdown ;
+  } 
 }
+  
+
+  
