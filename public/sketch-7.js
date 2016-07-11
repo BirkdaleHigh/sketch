015a28a1d45e59f1101e362a404f117ff6fd8a64
Jackbennett <jbennett@birkdalehigh.co.uk>
2016-07-11 12:42:22 +0100

    Swapped the order as one was coded later. inserted a different example

--- public/sketch-7.js
+++ public/sketch-7.js
@@ -5,55 +5,32 @@ var barlength = 0.5;
 var earlybar = 0;
 var col = 10
 
-var bg
-var list = []
-
 function setup() {
-  bg = color(20,90,90)
-  colorMode(HSB, 95)
   createCanvas(512, 512);
+  colorMode(HSB, 95)
   
-  background(bg)
-  fullscreen(true)
+    background(20,90,90)
 }
 
-var last = {'x': 0, 'y': 0}
-
 function draw() {
-  background(bg)
-  fill(255, 0, 0);
+  if (keyIsDown(LEFT_ARROW))
+    x-=5;
+
+  if (keyIsDown(RIGHT_ARROW))
+    x+=5;
+
+  if (keyIsDown(UP_ARROW))
+    y-=5;
+
+  if (keyIsDown(DOWN_ARROW))
+    y+=5;
+    
+  if (keyIsDown(67)){ // C
+    background(20,90,90)
+  }
   
-  if (keyIsDown(LEFT_ARROW)){
-    x -= 5
-  }
-
-  if (keyIsDown(RIGHT_ARROW)){
-    x += 5
-  }
-
-  if (keyIsDown(UP_ARROW)){
-    y -= 5
-  }
-
-  if (keyIsDown(DOWN_ARROW)){
-    y += 5
-  }
-
-  var circle = {'x': x, 'y': y}
-  if( (last.x !== x) || (last.y !== y) ){
-    console.log('last.x: ' + last.x + ' Circle.x: ' + circle.x + 'last.y: ' + last.y + ' Circle.y: ' + circle.y)
-    if(list.length < 40){
-      list.push(circle)
-    } else {
-      list.shift()
-      list.push(circle)
-    }
-  }
-
-  list.forEach(function(c){
-    ellipse(c.x, c.y ,5, 5)
-  })
-
-  last.x = circle.x
-  last.y = circle.y
+  console.log(keyCode)
+  
+  fill(255, 0, 0);
+  ellipse(x, y, 50, 50);
 }
