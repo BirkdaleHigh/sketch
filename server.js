bd2c0a25d748171a2b1d8addf36bf75f18ebee7e
Jackbennett <jbennett@birkdalehigh.co.uk>
2017-12-12 15:46:30 +0000

    Automatically template the list of sketches for the menu.

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express')
 var app = express()
 var http = require('http').Server(app)
 var os = require('os')
+var fs = require('fs')
 var handlebars = require('express-handlebars')
 var port  = process.env.PORT || 3000
 
@@ -12,6 +13,20 @@ app.engine('handlebars', handlebars({
 }));
 app.set('view engine', 'handlebars');
 
+var navList = fs.readdirSync('./public')
+  .filter(function(name){
+    return name.includes('sketch',0)
+  })
+  .map(function(name){
+    //select the first match of 1 or more digits
+    return name.match(/\d+/g)[0]
+  })
+  .sort(function(a,b){
+    var numA = parseInt(a, 10)
+    var numB = parseInt(b, 10)
+    return numA - numB
+  })
+
 app.get('/', function (req, res) {
   res.render('home');
 });
@@ -21,7 +36,7 @@ app.get('/sketch/:id', function (req, res, next) {
   console.log('requested url: ' + req.url)
   console.log('expanded filename: sketch-' + req.params.id + '.js')
   console.log('--------------')
-  res.render('sketch', {file: req.params.id})
+  res.render('sketch', {nav: navList, file: req.params.id})
 })
 
 http.listen(port, function () {
